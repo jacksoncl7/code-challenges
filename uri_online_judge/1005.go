297ae2e9a5c1062e6c3958de75e8a0eb77c17f1e
Jackson Candido De Lima <jackson.candido@g.globo>
2026-01-12 22:05:53 -0300

    init go mod

---
+++ uri_online_judge/1005.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func problem1005() {
+	var num1, num2 float64
+	fmt.Scan(&num1)
+	fmt.Scan(&num2)
+	avg := (num1*3.5 + num2*7.5) / 11
+	fmt.Printf("MEDIA = %.5f\n", avg)
+}
